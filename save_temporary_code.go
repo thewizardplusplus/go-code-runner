b03672d6405d3c26493dfe2abda7ea94b3cdf4b2
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-20 23:00:00 +0300

    Add the coderunner.SaveTemporaryCode() function

---
+++ save_temporary_code.go
@@ -0,0 +1,23 @@
+package coderunner
+
+import (
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+)
+
+// SaveTemporaryCode ...
+func SaveTemporaryCode(code string) (path string, err error) {
+	tempDir, err := ioutil.TempDir("", "code")
+	if err != nil {
+		return "", errors.Wrap(err, "unable to create a temporary directory")
+	}
+
+	tempFile := filepath.Join(tempDir, "code.go")
+	if err := ioutil.WriteFile(tempFile, []byte(code), 0600); err != nil {
+		return "", errors.Wrap(err, "unable to write the code")
+	}
+
+	return tempFile, nil
+}
