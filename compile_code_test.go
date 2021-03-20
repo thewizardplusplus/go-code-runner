b5c3bf504827adf1e0506cf5d6d0b13bb1ba750a
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-20 23:00:00 +0300

    Add tests for the coderunner.CompileCode() function

---
+++ compile_code_test.go
@@ -0,0 +1,34 @@
+package coderunner
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCompileCode(test *testing.T) {
+	const code = `package main; func main() { fmt.Println("Hello, World!") }`
+	pathToCode, err := SaveTemporaryCode(code)
+	require.NoError(test, err)
+
+	pathToExecutable, err := CompileCode(pathToCode)
+	require.NoError(test, err)
+
+	codeContent, err := ioutil.ReadFile(pathToCode)
+	require.NoError(test, err)
+
+	// we do not use filepath.Split() because it leaves the separator
+	dir, file := filepath.Dir(pathToExecutable), filepath.Base(pathToExecutable)
+	assert.Equal(test, os.TempDir(), filepath.Dir(dir))
+	assert.Regexp(test, `code\d+`, filepath.Base(dir))
+	assert.Equal(test, "code", file)
+
+	const wantedCodeContent = "package main\n\n" +
+		"import \"fmt\"\n\n" +
+		"func main() { fmt.Println(\"Hello, World!\") }\n"
+	assert.Equal(test, wantedCodeContent, string(codeContent))
+}
