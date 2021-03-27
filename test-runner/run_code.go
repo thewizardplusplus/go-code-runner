27180c6d051cb4c3c3f8a42b5e95ab19ac8b241f
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-27 23:00:00 +0300

    Add the testrunner.ErrTestCase.Error() method

--- test-runner/run_code.go
+++ test-runner/run_code.go
@@ -1,5 +1,9 @@
 package testrunner
 
+import (
+	"fmt"
+)
+
 // TestCase ...
 type TestCase struct {
 	Input          string
@@ -11,4 +15,13 @@ type ErrTestCase struct {
 	TestCase
 
 	ActualOutput string
+}
+
+// Error ...
+func (err ErrTestCase) Error() string {
+	return fmt.Sprintf(
+		"unexpected output: expected - %q, actual - %q",
+		err.ExpectedOutput,
+		err.ActualOutput,
+	)
 }
