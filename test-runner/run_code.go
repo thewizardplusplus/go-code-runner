0e54443d65012d550684f5a82fde99aa6425d545
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-27 23:00:00 +0300

    Add the testrunner.RunCode() function

--- test-runner/run_code.go
+++ test-runner/run_code.go
@@ -2,6 +2,9 @@ package testrunner
 
 import (
 	"fmt"
+
+	"github.com/pkg/errors"
+	coderunner "github.com/thewizardplusplus/go-code-runner"
 )
 
 // TestCase ...
@@ -24,4 +27,23 @@ func (err ErrTestCase) Error() string {
 		err.ExpectedOutput,
 		err.ActualOutput,
 	)
+}
+
+// RunCode ...
+func RunCode(pathToExecutable string, testCases []TestCase) error {
+	for _, testCase := range testCases {
+		actualOutput, err := coderunner.RunCode(pathToExecutable, testCase.Input)
+		if err != nil {
+			return errors.Wrapf(
+				err,
+				"unable to run the test case (input - %q)",
+				testCase.Input,
+			)
+		}
+		if actualOutput != testCase.ExpectedOutput {
+			return ErrTestCase{TestCase: testCase, ActualOutput: actualOutput}
+		}
+	}
+
+	return nil
 }
