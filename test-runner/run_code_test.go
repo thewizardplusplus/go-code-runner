ceacb0d498a0929fa7d5322d3d4302b3959fbb7d
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-03-27 23:00:00 +0300

    Add basics of tests for the testrunner.RunCode() function

--- test-runner/run_code_test.go
+++ test-runner/run_code_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	coderunner "github.com/thewizardplusplus/go-code-runner"
 )
 
 func TestErrTestCase_Error(test *testing.T) {
@@ -15,4 +17,32 @@ func TestErrTestCase_Error(test *testing.T) {
 	const wantedErrMessage = "unexpected output: " +
 		`expected - "expected output", actual - "actual output"`
 	assert.EqualError(test, err, wantedErrMessage)
+}
+
+func TestRunCode(test *testing.T) {
+	type args struct {
+		code      string
+		testCases []TestCase
+	}
+
+	for _, data := range []struct {
+		name      string
+		args      args
+		wantedErr assert.ErrorAssertionFunc
+	}{
+		// TODO: Add test cases.
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			pathToCode, err := coderunner.SaveTemporaryCode(data.args.code)
+			require.NoError(test, err)
+
+			pathToExecutable, err := coderunner.CompileCode(pathToCode)
+			require.NoError(test, err)
+
+			receivedErr := RunCode(pathToExecutable, data.args.testCases)
+			require.NoError(test, err)
+
+			data.wantedErr(test, receivedErr)
+		})
+	}
 }
