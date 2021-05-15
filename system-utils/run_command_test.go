3a4972d45c912735afb03cbc21fe166b4da4a874
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-15 23:00:00 +0300

    Implement tests of the systemutils.RunCommand() function

--- system-utils/run_command_test.go
+++ system-utils/run_command_test.go
@@ -21,7 +21,61 @@ func TestRunCommand(test *testing.T) {
 		wantedOutput string
 		wantedErr    assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success without an input",
+			args: args{
+				ctx:       context.Background(),
+				input:     "",
+				command:   "echo",
+				arguments: []string{"test"},
+			},
+			wantedOutput: "test\n",
+			wantedErr:    assert.NoError,
+		},
+		{
+			name: "success with an input",
+			args: args{
+				ctx:       context.Background(),
+				input:     "test",
+				command:   "cat",
+				arguments: nil,
+			},
+			wantedOutput: "test",
+			wantedErr:    assert.NoError,
+		},
+		{
+			name: "error without a stderr message",
+			args: args{
+				ctx:       context.Background(),
+				input:     "",
+				command:   "non-existent",
+				arguments: nil,
+			},
+			wantedOutput: "",
+			wantedErr:    assert.Error,
+		},
+		{
+			name: "error with an empty stderr message",
+			args: args{
+				ctx:       context.Background(),
+				input:     "",
+				command:   "false",
+				arguments: nil,
+			},
+			wantedOutput: "",
+			wantedErr:    assert.Error,
+		},
+		{
+			name: "error with a non-empty stderr message",
+			args: args{
+				ctx:       context.Background(),
+				input:     "",
+				command:   "cat",
+				arguments: []string{"non-existent"},
+			},
+			wantedOutput: "",
+			wantedErr:    assert.Error,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			receivedOutput, receivedErr := RunCommand(
