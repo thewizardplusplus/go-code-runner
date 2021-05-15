b1e059a96be9225eb7c518dc3f26914f743b72c7
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-05-15 23:00:00 +0300

    Use the testrunner.TestCaseRunner type in the testrunner.RunTestCases() function

--- test-runner/run_test_cases.go
+++ test-runner/run_test_cases.go
@@ -2,8 +2,6 @@ package testrunner
 
 import (
 	"context"
-
-	systemutils "github.com/thewizardplusplus/go-code-runner/system-utils"
 )
 
 // TestCase ...
@@ -21,12 +19,11 @@ type TestCaseRunner func(ctx context.Context, input string) (
 // RunTestCases ...
 func RunTestCases(
 	ctx context.Context,
-	pathToExecutable string,
 	testCases []TestCase,
+	testCaseRunner TestCaseRunner,
 ) error {
 	for _, testCase := range testCases {
-		actualOutput, err :=
-			systemutils.RunCommand(ctx, testCase.Input, pathToExecutable)
+		actualOutput, err := testCaseRunner(ctx, testCase.Input)
 		if err != nil {
 			return ErrFailedRunning{TestCase: testCase, ErrMessage: err.Error()}
 		}
